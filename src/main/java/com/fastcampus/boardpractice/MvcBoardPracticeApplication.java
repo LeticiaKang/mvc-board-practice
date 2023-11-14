d782e6920bda3c7f9ec90e2dc15a503f1b7276b0
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-11-14 16:27:04 +0900

    #3 - 스프링부트 프로젝트 시작
    스프링부트 프로젝트 초기화
    들어간 초기화 기술:
    - 스프링웹
    - 액츄에이터
    - 데브툴즈
    - 롬보

---
+++ src/main/java/com/fastcampus/boardpractice/MvcBoardPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBoardPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBoardPracticeApplication.class, args);
+	}
+
+}
