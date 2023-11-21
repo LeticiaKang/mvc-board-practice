8562cba7002a1252c1f50b406d55be376fe64eb5
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-11-21 16:01:30 +0900

    #5 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    'QuerydslPredicateExecutor', 'QuerydslBinderCustomizer'를 이용함.
    - QuerydslPredicateExecutor : 모든 필드에 대해 검색 기능을 지원(정확한 검색만 가능)
    - QuerydslBinderCustomizer : 다양한 기능(부분 검색, 대소문자 무시 등)을 마음대로 추가 설정할 수 있도록 지

--- src/main/java/com/fastcampus/boardpractice/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/boardpractice/repository/ArticleRepository.java
@@ -1,10 +1,31 @@
 package com.fastcampus.boardpractice.repository;
 
 import com.fastcampus.boardpractice.domain.Article;
+import com.fastcampus.boardpractice.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); //선택적인 필드를 지정할 수 있다.
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); //원하는 필드를 추가한다.
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); //대소문자 구분하지 않음(편리성을 위해 likeIgnoreCase를 쓰지 않음)
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
