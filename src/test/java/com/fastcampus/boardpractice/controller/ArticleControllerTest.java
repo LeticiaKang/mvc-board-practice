a5e93c22090feb59c9c37886afd36f596c3667ea
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-11-27 20:48:19 +0900

    #17 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    - 스프링 Security가 자동 생성해준 '/login' 컨트롤러를 테스트 함.
    - 모든 컨트롤러가 Security 설정을 부를 수 있도록
    '@Import' 추가

--- src/test/java/com/fastcampus/boardpractice/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/boardpractice/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.fastcampus.boardpractice.controller;
 
+import com.fastcampus.boardpractice.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class) //Security설정이 모두 통과하도록 설정
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -48,7 +51,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articleComments"));
     }
 
-    @Disabled("구현 중")
+    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
@@ -61,7 +64,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles/search"));
     }
 
-    @Disabled("구현 중")
+    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
