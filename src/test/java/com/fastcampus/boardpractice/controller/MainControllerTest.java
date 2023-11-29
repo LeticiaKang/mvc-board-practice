1ab9061119fd708857261a549420ea5f870eafac
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-11-29 11:50:29 +0900

    #20 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현
    MainController 를 만들어서 기본 페이지가 게시판 페이지가 되도록 함.
    강의 중에서는 redirect 로 접근했는데,
    단순 조회 접근이고, url 루트 경로를 유지한 채로 게시판 페이지를대표로 만들고 싶은 상황이므로 forward 가 더 적절해보여서 수정함

---
+++ src/test/java/com/fastcampus/boardpractice/controller/MainControllerTest.java
@@ -0,0 +1,38 @@
+package com.fastcampus.boardpractice.controller;
+
+import com.fastcampus.boardpractice.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("main 컨트롤러 - home버튼")
+@Import(SecurityConfig.class) //Security설정이 모두 통과하도록 설정
+@WebMvcTest(ArticleController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingRootPage_thenReturnsArticlesPage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+}
